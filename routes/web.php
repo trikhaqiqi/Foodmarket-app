665d4dc669346ff6f861906bb46de8b13b4cfa52
ilyassstri <trikhaqiqi@gmail.com>
2021-12-23 09:18:09 +0700

    add API Checkout with Midtrans

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/debug-sentry', function() {
+    throw new Exception('My first Sentry error!');
+});
+ 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
