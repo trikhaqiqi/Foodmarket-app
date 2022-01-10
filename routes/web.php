8764f7eb39001d182a76466ab109633541a23053
ilyassstri <trikhaqiqi@gmail.com>
2022-01-11 06:21:29 +0700

    modify web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\API\MidtransController;
 
 /*
@@ -31,6 +32,10 @@ Route::prefix('dashboard')
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
         Route::resource('food', FoodController::class);
+
+        Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])
+            ->name('transactions.changeStatus');
+        Route::resource('transactions', TransactionController::class);
     });
 
 // Route::get('/debug-sentry', function() {
